a1f45fcd837964c507e041bdf9401e57b8e23753
Nipun Tharuka <tharukan009@gmail.com>
2025-04-23 22:36:57 +0530

    mails changes

--- components/application/EmailCard.tsx
+++ components/application/EmailCard.tsx
@@ -3,22 +3,24 @@ import {CheckIcon, EnvelopeIcon, EnvelopeOpenIcon} from '@heroicons/react/24/out
 export default function EmailCard({email, onOpenStatusChange}: any) {
     return (
         <div className="bg-white shadow-lg rounded-lg p-4 flex flex-col gap-2 hover:shadow-xl transition-shadow">
-            <div className="flex items-center gap-3">
+            <div className="flex items-center justify-between gap-3">
                 <div onClick={() => onOpenStatusChange(email)} className="cursor-pointer">
                     {email.open ? (
-                        <EnvelopeOpenIcon className="h-6 w-6 text-gray-500" style={{ strokeWidth: 2 }}/>
+                        <EnvelopeOpenIcon className="h-6 w-6 text-gray-500" style={{strokeWidth: 2}}/>
                     ) : (
-                        <EnvelopeIcon className="h-6 w-6  text-blue-500" style={{ strokeWidth: 3 }}/>
+                        <EnvelopeIcon className="h-6 w-6  text-blue-500" style={{strokeWidth: 3}}/>
                     )}
                 </div>
-                <a href={`mailto:${email?.email}`} className="font-semibold text-blue-700 overflow-x-auto">{email.email}</a>
-                {email.read && (<CheckIcon className="h-6 w-6 text-green-600"  style={{ strokeWidth: 5 }}/>)}
+                <div className="text-lg text-gray-400">
+                    <span>{email.firstName + " " + email.lastName}</span>
+                </div>
+                {email.read && (<CheckIcon className="h-6 w-6 text-green-600" style={{strokeWidth: 5}}/>)}
             </div>
+            <a href={`mailto:${email?.email}`} className="font-semibold text-sm text-center text-blue-700 overflow-x-auto">{email.email}</a>
             <div className="text-gray-600 text-sm">{email.message}</div>
             <div className="mt-2">
-                <a href={`tel:${email?.phone}`} className="text-[16px] text-gray-300">{email?.phone}</a>
-                <div className="flex justify-between items-center text-xs text-gray-400">
-                    <span>{email.firstName + " " + email.lastName}</span>
+                <div className="flex justify-between items-center text-xs text-gray-300">
+                    <a href={`tel:${email?.phone}`}>{email?.phone}</a>
                     <span>{new Date(email.createdAt).toLocaleDateString()}</span>
                 </div>
             </div>
